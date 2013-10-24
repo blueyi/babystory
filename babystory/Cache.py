24a85c9bcb1a786295336117dc3597c3dea23807
LiuLang <gsushzhsosgsu@gmail.com>
2013-10-25 01:52:38 +0800

    Fixed: continue to download next song when failed to download current song

--- babystory/Cache.py
+++ babystory/Cache.py
@@ -6,16 +6,31 @@
 
 from gi.repository import GLib
 from gi.repository import Gtk
+import os
 
-from babystory import Widgets
+from babystory import Config
 from babystory import Net
+from babystory import Widgets
 
-CHOOSE, TITLE, SIZE, DURATION, URL, PERCENT = list(range(6))
+_ = Config._
+
+TITLE, SIZE, DURATION, URL, CATEGORY, CHOOSE, PERCENT = list(range(7))
+
+def song_row_to_dict(song_row):
+    song = {
+            'Title': song_row[TITLE],
+            'Size': song_row[SIZE],
+            'Duration': song_row[DURATION],
+            'Url': song_row[URL],
+            'Category': song_row[CATEGORY],
+            }
+    return song
+
 
 class Cache(Gtk.Dialog):
     def __init__(self, app):
         self.app = app
-        super().__init__('Caching job', app.window, 0,
+        super().__init__(_('Caching job'), app.window, 0,
                 (Gtk.STOCK_CLOSE, Gtk.ResponseType.CLOSE, ))
         self.set_modal(True)
         self.set_transient_for(app.window)
@@ -26,7 +41,7 @@ class Cache(Gtk.Dialog):
         box = self.get_content_area()
         box.set_spacing(5)
 
-        control_button = Gtk.Button('Start Caching')
+        control_button = Gtk.Button(_('Start Caching'))
         control_button.download = False
         control_button.connect('clicked', self.on_control_button_clicked)
         box.pack_start(control_button, False, False, 0)
@@ -35,15 +50,15 @@ class Cache(Gtk.Dialog):
         songs_win = Gtk.ScrolledWindow()
         box.pack_start(songs_win, True, True, 0)
 
-        # choose, title, size, duration, url, percent
-        self.liststore = Gtk.ListStore(bool, str, str, str, str, int)
+        # title, size, duration, url, category, choose, category
+        self.liststore = Gtk.ListStore(str, str, str, str, str, bool, int)
         songs_tv = Gtk.TreeView(model=self.liststore)
         songs_win.add(songs_tv)
         self.songs_tv = songs_tv
 
         choose_cell = Gtk.CellRendererToggle()
         choose_cell.connect('toggled', self.on_choose_cell_toggled)
-        choose_col = Gtk.TreeViewColumn('Choose', choose_cell,
+        choose_col = Gtk.TreeViewColumn(_('Choose'), choose_cell,
                 active=CHOOSE)
         choose_col.props.clickable = True
         choose_col.connect('clicked', self.on_choose_col_clicked)
@@ -54,26 +69,40 @@ class Cache(Gtk.Dialog):
         songs_tv.append_column(choose_col)
 
         title_cell = Gtk.CellRendererText()
-        title_col = Widgets.ExpandedTreeViewColumn('Title', title_cell,
+        title_col = Widgets.ExpandedTreeViewColumn(_('Title'), title_cell,
                 text=TITLE)
         songs_tv.append_column(title_col)
 
         proc_cell = Gtk.CellRendererText()
-        proc_col = Gtk.TreeViewColumn('Process', proc_cell, text=PERCENT)
+        proc_col = Gtk.TreeViewColumn(_('Process'), proc_cell, text=PERCENT)
         songs_tv.append_column(proc_col)
 
         size_cell = Gtk.CellRendererText()
-        size_col = Gtk.TreeViewColumn('Size', size_cell, text=SIZE)
+        size_col = Gtk.TreeViewColumn(_('Size'), size_cell, text=SIZE)
         songs_tv.append_column(size_col)
 
         duration_cell = Gtk.CellRendererText()
-        duration_col = Gtk.TreeViewColumn('Duration', duration_cell,
+        duration_col = Gtk.TreeViewColumn(_('Duration'), duration_cell,
                 text=DURATION)
         songs_tv.append_column(duration_col)
 
+        index = 0
         for song in app.playlist.right_liststore:
-            self.liststore.append([True, song[0], song[1], song[2],
-                song[3], 0, ])
+            song_obj = self.app.playlist.get_song_from_index(index)
+            song_path = Net.get_song_path(self.app, song_obj)
+            index += 1
+            if os.path.exists(song_path):
+                print('song exists:', song_path)
+                continue
+            self.liststore.append([
+                song[TITLE],
+                song[SIZE],
+                song[DURATION],
+                song[URL],
+                song[CATEGORY],
+                True,
+                0,
+                ])
 
         self.curr_index = 0
         self.async_job = None
@@ -90,15 +119,15 @@ class Cache(Gtk.Dialog):
         print('Cache.on_control_button_clicked()', button)
         if self.curr_index >= len(self.liststore) - 1:
             print('all songs are downloaded')
-            self.control_button.set_label('Finished')
+            self.control_button.set_label(_('Finished'))
             return
 
         if self.async_job is None:
-            button.set_label('Stop Caching')
+            button.set_label(_('Stop Caching'))
             button.download = True
             self.cache_song()
         else:
-            button.set_label('Start Caching')
+            button.set_label(_('Start Caching'))
             button.download = False
             self.async_job.destroy()
             self.async_job = None
@@ -119,8 +148,17 @@ class Cache(Gtk.Dialog):
         GLib.idle_add(_update_process)
 
     def on_song_downloaded(self, widget, song_path):
+        def _remove_song():
+            path = Gtk.TreePath(self.curr_index)
+            self.liststore.remove(self.liststore.get_iter(path))
+            self.curr_index -= 1
+            self.cache_next_song()
+
         print('Cache.on_song_downloaded()', song_path)
-        if song_path:
+        if not song_path:
+            # remove this item from liststore
+            GLib.idle_add(_remove_song)
+        else:
             GLib.idle_add(self.cache_next_song)
 
     def cache_next_song(self):
@@ -136,9 +174,14 @@ class Cache(Gtk.Dialog):
     def cache_song(self):
         path = Gtk.TreePath(self.curr_index)
         self.songs_tv.get_selection().select_path(path)
-        song = self.app.playlist.get_song_from_index(self.curr_index)
+        song = self.get_song_from_index(self.curr_index)
 
         self.async_job = Net.AsyncSong(self.app)
         self.async_job.connect('chunk-received', self.on_chunk_received)
         self.async_job.connect('downloaded', self.on_song_downloaded)
         self.async_job.get_song(song)
+
+    def get_song_from_index(self, index):
+        row = self.liststore[index]
+        song = song_row_to_dict(row)
+        return song
